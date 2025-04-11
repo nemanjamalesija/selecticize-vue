c2c25183069b77cd9e6ae9e7cc3412e8bbd9989d
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-04-12 01:39:15 +0200

    Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,180 @@
+import configBase from 'eslint-config-nitpick';
+import configTypescript from 'eslint-config-nitpick/typescript';
+import configBrowser from 'eslint-config-nitpick/browser';
+import configVue from 'eslint-config-nitpick/vue';
+import configPrettier from 'eslint-config-prettier';
+import pluginImport from 'eslint-plugin-import';
+import pluginPrettier from 'eslint-plugin-prettier';
+import pluginJsdoc from 'eslint-plugin-jsdoc';
+import pluginUnicorn from 'eslint-plugin-unicorn';
+import globals from 'globals';
+
+export default [
+	configBase,
+	configTypescript,
+	...configVue,
+	configPrettier,
+	{ languageOptions: { globals: globals.browser } },
+	{
+		plugins: {
+			import: pluginImport,
+			prettier: pluginPrettier,
+			jsdoc: pluginJsdoc,
+			unicorn: pluginUnicorn
+		},
+		'rules': {
+			'no-use-before-define': 0,
+			'import/prefer-default-export': 0,
+			'node/prefer-global/url': 0,
+			'unicorn/explicit-length-check': 0,
+			'prettier/prettier': 1,
+			'no-promise-executor-return': 0,
+			'no-restricted-globals': [
+				2,
+				{
+					'name': 'global',
+					'message': 'Use `globalThis`.'
+				}
+			],
+			'import/order': [
+				1,
+				{
+					'pathGroups': [
+						{
+							'pattern': 'vue',
+							'group': 'external',
+							'position': 'before'
+						}
+					],
+					'pathGroupsExcludedImportTypes': ['app/**', 'pb/**'],
+					'newlines-between': 'never'
+				}
+			],
+			'import/no-namespace': 0,
+			'import/exports-last': 0,
+			'import/group-exports': 0,
+			'import/dynamic-import-chunkname': 0,
+			'object-shorthand': 0,
+			'prefer-object-spread': 0,
+			'capitalized-comments': 0,
+			'quote-props': 0,
+			'multiline-comment-style': 0,
+			'no-useless-return': 0,
+			'dot-notation': 0,
+			'unicorn/filename-case': [
+				2,
+				{
+					'case': 'kebabCase'
+				}
+			],
+			'unicorn/prefer-add-event-listener': 0,
+			'unicorn/prevent-abbreviations': 0,
+			'jsdoc/require-description-complete-sentence': 0,
+			'unicorn/prefer-string-slice': 0,
+			'unicorn/prefer-dataset': 0,
+			'unicorn/prefer-dom-node-dataset': 0,
+			'unicorn/no-lonely-if': 0,
+			'unicorn/prefer-spread': 0,
+			'unicorn/prefer-flat-map': 0,
+			'unicorn/prefer-at': 0,
+			'unicorn/prefer-object-has-own': 0,
+			'unicorn/prefer-object-from-entries': 0,
+			'prefer-object-has-own': 0,
+			'unicorn/regex-shorthand': 0,
+			'no-useless-escape': 0,
+			'import/named': 2,
+			'import/default': 2,
+			'no-constant-binary-expression': 0,
+			'unicorn/switch-case-braces': 0,
+			'unicorn/prefer-native-coercion-functions': 0,
+			'unicorn/no-useless-promise-resolve-reject': 0,
+			'unicorn/no-await-expression-member': 0,
+			'unicorn/prefer-module': 2,
+			'import/no-commonjs': 2,
+			'import/no-unresolved': 2,
+			'n/hashbang': 0,
+			'unicorn/no-keyword-prefix': 0
+		}
+	},
+
+	{
+		'files': ['src/**/*'],
+		'rules': {
+			'import/no-nodejs-modules': 2,
+			'vue/v-if-else-key': 0,
+			'vue/require-default-prop': 0,
+			'vue/new-line-between-multi-line-property': 0,
+			'vue/no-multiple-template-root': 0,
+			'vue/no-v-for-template-key': 0,
+			'vue/v-on-event-hyphenation': 0,
+			'vue/custom-event-name-casing': 0,
+			'vue/no-reserved-component-names': 0,
+			'vue/require-explicit-emits': 0,
+			'vue/prefer-prop-type-boolean-first': 0,
+			'vue/no-static-inline-styles': 0,
+			'vue/no-undef-components': [
+				2,
+				{
+					'ignorePatterns': ['RouterView', 'RouterLink']
+				}
+			],
+			'vue/component-name-in-template-casing': [
+				2,
+				'PascalCase',
+				{
+					'ignores': [
+						'svg',
+						'rect',
+						'component',
+						'transition',
+						'transition-group',
+						'keep-alive',
+						'slot'
+					]
+				}
+			],
+			'vue/order-in-components': [
+				2,
+				{
+					'order': [
+						'el',
+						'name',
+						'key',
+						'parent',
+						'functional',
+						['delimiters', 'comments'],
+						['components', 'directives', 'filters'],
+						'extends',
+						'mixins',
+						['provide', 'inject'],
+						'ROUTER_GUARDS',
+						'layout',
+						'validate',
+						'scrollToTop',
+						'transition',
+						'loading',
+						'inheritAttrs',
+						'model',
+						['props', 'propsData'],
+						'emits',
+						'setup',
+						'data',
+						'fetch',
+						'head',
+						'computed',
+						'watch',
+						'watchQuery',
+						'LIFECYCLE_HOOKS',
+						'methods',
+						['template', 'render'],
+						'renderError'
+					]
+				}
+			]
+		}
+	},
+
+	{
+		ignores: ['**/*.json']
+	}
+];
