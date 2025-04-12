a9ff3b3e37b72518e25ecfa5f7bbc0c3e6fc8fce
Nemanja Malesija <malesija.nemanja@gmail.com>
2025-04-12 02:44:37 +0200

    Create app.vue

---
+++ src/app.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="cnt">
+		<Selecticize
+			:sources="sources"
+			:maxInputs="4"
+			:index="1"
+			placeholder="bla"
+			payloadKey="kind"
+		/>
+	</div>
+</template>
+<script setup>
+import { ref } from 'vue';
+import Selecticize from './components/selecticize.vue';
+
+const sources = [
+	{ id: 'consumption', title: 'Graf potrošnje' },
+	{ id: 'correlation', title: 'Korelacija' },
+	{ id: 'regression', title: 'Regresivna Analiza' }
+];
+</script>
